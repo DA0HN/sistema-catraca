c1f7a806476dfd85612b8479b777adeaa116bfeb
Gabriel Honda <gabrielhondacba@gmail.com>
2021-03-21 13:27:36 -0400

    wip: adiciona tela de transferência de dados

---
+++ catraca-control-desktop/src/main/java/br/edu/ifmt/catracacontrol/controllers/TransferDataController.java
@@ -0,0 +1,20 @@
+package br.edu.ifmt.catracacontrol.controllers;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransferDataController implements Initializable {
+
+  private Stage stage;
+
+  @Override public void initialize(URL location, ResourceBundle resources) {
+
+  }
+
+  public void setStage(Stage stage) {
+    this.stage = stage;
+  }
+}
---
+++ catraca-control-desktop/src/main/java/br/edu/ifmt/catracacontrol/views/TransferDataView.java
@@ -0,0 +1,34 @@
+package br.edu.ifmt.catracacontrol.views;
+
+import br.edu.ifmt.catracacontrol.controllers.HomeController;
+import br.edu.ifmt.catracacontrol.controllers.TransferDataController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TransferDataView extends Application {
+  @Override public void start(Stage stage) throws Exception {
+    try {
+      var view = new FXMLLoader();
+      Parent parent = view.load(getClass().getResourceAsStream("/views/transfer_data.fxml"));
+      parent.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+      TransferDataController controller = view.getController();
+      controller.setStage(stage);
+      stage.setScene(new Scene(parent));
+      stage.setTitle("CATRACA $YSTEM");
+      stage.setResizable(false);
+      stage.getIcons().add(
+        new Image(this.getClass().getResourceAsStream("/images/ticket_gate.png"))
+      );
+      stage.show();
+    }
+    catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
