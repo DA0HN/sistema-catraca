817a083e44c55a7a0b9e93cf273dac663f8623f2
Gabriel Honda <gabrielhondacba@gmail.com>
2021-03-21 14:07:43 -0400

    chore: adiciona JSerialComm

--- catraca-control-desktop/src/main/java/module-info.java
+++ catraca-control-desktop/src/main/java/module-info.java
@@ -1,10 +1,15 @@
 module catraca.control.desktop {
-  // Javafx
+  // Interface gráfica
   requires javafx.controls;
   requires javafx.fxml;
-  requires com.jfoenix;
-
   requires java.desktop;
+  requires com.jfoenix;
+  // Ícones da UI
+  requires de.jensd.fx.glyphs.fontawesome;
+  requires de.jensd.fx.glyphs.commons;
+
+  // Comunicação Serial
+  requires com.fazecast.jSerialComm;
 
   // Lombok impl
   requires lombok;
@@ -14,13 +19,14 @@ module catraca.control.desktop {
   requires java.sql;
   requires java.persistence;
   requires java.base;
-  requires de.jensd.fx.glyphs.fontawesome;
-  requires de.jensd.fx.glyphs.commons;
 
+  // Abre os pacotes para Reflection
   opens br.edu.ifmt.catracacontrol;
   opens br.edu.ifmt.catracacontrol.controllers;
   opens br.edu.ifmt.catracacontrol.views;
   opens br.edu.ifmt.catracacontrol.domain.dto;
   opens br.edu.ifmt.catracacontrol.domain.models;
   opens br.edu.ifmt.catracacontrol.domain.repositories;
+  opens br.edu.ifmt.catracacontrol.domain.services;
+  opens br.edu.ifmt.catracacontrol.domain.services.serialcommunication;
 }
