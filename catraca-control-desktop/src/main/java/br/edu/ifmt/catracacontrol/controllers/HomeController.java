1b3aebacabc3d0dba4bc909681195512ef2783df
Gabriel Honda <gabrielhondacba@gmail.com>
2021-03-21 13:26:22 -0400

    feat: adiciona botão para tela de comunicação serial

--- catraca-control-desktop/src/main/java/br/edu/ifmt/catracacontrol/controllers/HomeController.java
+++ catraca-control-desktop/src/main/java/br/edu/ifmt/catracacontrol/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package br.edu.ifmt.catracacontrol.controllers;
 
 import br.edu.ifmt.catracacontrol.views.CreateView;
+import br.edu.ifmt.catracacontrol.views.TransferDataView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,6 +17,7 @@ public class HomeController implements Initializable {
   private Stage stage;
   @FXML private Button viewClientButton;
   @FXML private Button createClientButton;
+  @FXML private Button serialCommunicationButton;
 
   @FXML private void createClientButtonOnClick(MouseEvent event) throws Exception {
     var createView = new CreateView();
@@ -22,11 +25,21 @@ public class HomeController implements Initializable {
   }
 
   @FXML private void viewClientButtonOnClick(MouseEvent event) {
+    JOptionPane.showMessageDialog(
+      new JFrame(),
+      "Novidades em breve",
+      "Em construção",
+      JOptionPane.INFORMATION_MESSAGE
+    );
+  }
 
+  @FXML
+  private void serialCommunicationButtonOnClick(MouseEvent event) throws Exception {
+    var transferDataView = new TransferDataView();
+    transferDataView.start(stage);
   }
 
   @Override public void initialize(URL url, ResourceBundle resourceBundle) {
-
   }
 
   public void setStage(Stage stage) {
